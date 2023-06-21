36474cbf2fe42fe58915a2ff29bfa773089722f1
Noel Jacob <noeljacob91@gmail.com>
2023-06-21 12:33:39 +0530

    try: fixing db insertion 2

--- src/routes/log/+server.ts
+++ src/routes/log/+server.ts
@@ -2,7 +2,7 @@ import {error, json} from '@sveltejs/kit';
 import type {RequestHandler} from './$types';
 import {drizzle} from "drizzle-orm/planetscale-serverless";
 import {connect} from "@planetscale/database";
-import * as schema from "../../db/schema";
+import {debug} from "../../db/schema";
 // @ts-ignore
 import {DB_HOST, DB_PASSWORD, DB_USERNAME} from '$env/static/private';
 
@@ -11,7 +11,7 @@ const connection = connect({
     host: DB_HOST, username: DB_USERNAME, password: DB_PASSWORD,
 });
 
-const db = drizzle(connection, {schema});
+const db = drizzle(connection, {schema: {debug}});
 
 export const POST: RequestHandler = async ({request, getClientAddress}) => {
     const body = await request.json()
@@ -25,7 +25,9 @@ export const POST: RequestHandler = async ({request, getClientAddress}) => {
             const info = {
                 client: body.data, ip
             }
-            await db.insert(schema.debug).values({data: info}).execute().catch(e => error(501, JSON.stringify(e)));
+            await db.insert(debug).values({data: info}).execute().catch(e => {
+                throw error(501, JSON.stringify(e))
+            });
             return json({status: "added to db"});
 
         case "click":
