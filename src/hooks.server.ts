59a330dc78c562b5a1ba704870847b4913763493
Noel Jacob <noeljacob91@gmail.com>
2023-06-22 15:28:14 +0530

    fix: sentry 3

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,5 @@
 import type {HandleServerError} from '@sveltejs/kit';
-import {init, sentryHandle, handleErrorWithSentry, } from "@sentry/sveltekit";
+import {init, handleErrorWithSentry, } from "@sentry/sveltekit";
 
 init({
     dsn: "https://4406d098019e498989287e43ca98038d@o463075.ingest.sentry.io/4505401534840832",
@@ -9,6 +9,6 @@ init({
     tracesSampleRate: 1.0,
 });
 
-export const handle = sentryHandle();
+// export const handle = sentryHandle();
 
 export const handleError = handleErrorWithSentry();
