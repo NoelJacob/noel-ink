cabd245028588287e4d9ea6edf925b5b5e2b6c71
Noel Jacob <noeljacob91@gmail.com>
2023-06-22 15:27:01 +0530

    fix: sentry 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import type { UserConfig } from "vite";
-import { sentrySvelteKit } from "@sentry/sveltekit";
+// import { sentrySvelteKit } from "@sentry/sveltekit";
 
 const config: UserConfig = {
-  plugins: [sentrySvelteKit(),sveltekit()],
+  plugins: [
+      // sentrySvelteKit(),
+    sveltekit()
+  ],
 };
 
 export default config;
